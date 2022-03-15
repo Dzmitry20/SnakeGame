04c3919a8904ce2eb8b4dc1fd926975b947b5e68
Dzmitry <zhdv27@mail.ru>
2022-03-15 19:56:55 +0300

    second commit

--- src/com/company/GameFrame.java
+++ src/com/company/GameFrame.java
@@ -3,4 +3,16 @@ package com.company;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
+
+    GameFrame(){
+        GamePanel panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setSize(550,550);
+    }
 }
